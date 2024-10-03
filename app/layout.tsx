63a74e1aa2758ba21f750c569c5b2d624d692f52
rk-akaike <ramakrishna@akaiketech.com>
2024-10-03 10:46:33 +0530

    Refactor layout component to update Google Tag Manager implementation and remove unused metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-"use client";
-
-import { useEffect } from "react";
 import Script from "next/script";
 import localFont from "next/font/local";
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -16,27 +14,22 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+export const metadata = {
+  title: "Conversion Tracker",
+  description: "Conversion tracking specially for Muwaz",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function gtag(...args: [string, ...unknown[]]) {
-      window.dataLayer.push(args);
-    };
-    window.gtag("js", new Date());
-    window.gtag("config", "GTM-NKWDQD7T");
-  }, []);
-
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=GTM-NKWDQD7T`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-7WFYRDDRL5`}
         />
         <Script
           id="gtag-init"
@@ -46,7 +39,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(...args) { dataLayer.push(args); }
               gtag('js', new Date());
-              gtag('config', 'GTM-NKWDQD7T');
+              gtag('config', 'G-7WFYRDDRL5');
             `,
           }}
         />
